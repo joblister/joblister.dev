fe5f94f16636e123f2431f15348f2daaabcc4453
Donald Moore <donmoore26762671@gmail.com>
2016-03-31 09:55:19 -0500

    Folder setup for joblister project. Created index.html,user_migration, users_seeder.

---
+++ Input.php
@@ -0,0 +1,78 @@
+<?php
+
+class Input
+{
+
+  
+
+    public static function has($key){
+
+       return isset($_REQUEST[$key]);
+        
+    }
+    
+    public static function get($key, $default = null)
+    {
+        return isset($_REQUEST[$key])? $_REQUEST[$key]: $default;
+    }
+
+
+    public static function escape($input){
+
+        return htmlspecialchars(strip_tags($input));
+
+
+    }
+
+    public static function getNumber($key){
+
+        $value = self::get($key);
+
+        if(!is_numeric($value) || $value = null){
+
+            throw new Exception('Your input is not a number or null!');
+
+        }
+           
+            $value = (float)self::get($key);
+            return $value;
+
+    }
+
+    public static function getDate($key){
+        $value = self::get($key);
+
+        $validDate = date_create($value);
+
+        if($validDate) {
+            return $value;
+        }else {
+            throw new Exception("The input provided is not in a valid date format");
+        }
+    }
+    
+    public static function getString($key){
+
+           $value = self::get($key);
+
+          if( ($value == null) || (is_resource($value)) || (is_numeric($value)) || (is_bool($value))
+                || (is_array($value)) || (is_object($value)) ){
+
+             throw new Exception('Your input is not a string or null!' . $key);
+
+        }
+
+            
+            return $value;
+          
+   
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    //                      DO NOT EDIT ANYTHING BELOW!!                     //
+    // The Input class should not ever be instantiated, so we prevent the    //
+    // constructor method from being called. We will be covering private     //
+    // later in the curriculum.                                              //
+    ///////////////////////////////////////////////////////////////////////////
+    private function __construct() {}
+}
---
+++ users_seeder.php
@@ -0,0 +1,29 @@
+<?php
+require 'joblister_db_connect.php';
+require 'db_connect.php';
+// First, add a query to delete all the records from the table
+
+$truncate = 'TRUNCATE users';
+$dbc->exec($truncate);
+
+$allUsers = [
+    ['first_name' => 'Don', 'last_name' => 'Moore','user_name' => 'donny71','email' => 'dmoore@jlister'
+     'password' => password_hash('seetheworld1971', PASSWORD_DEFAULT) ],
+
+     ['first_name' => 'Richie', 'last_name' => 'DeLosSantos','user_name' => 'Richard1292','email' => 'richie@jlister'
+     'password' => password_hash('richierich', PASSWORD_DEFAULT) ],
+
+     ['first_name' => 'Christian', 'last_name' => 'Laettner','user_name' => 'buzzerbeater','email' => 'chris@dukeuni.com'
+     'password' => password_hash('beatunc', PASSWORD_DEFAULT) ],
+];
+$stmt = $dbc->prepare('INSERT INTO users (first_name, last_name, user_name, email, password)
+    VALUES (:first_name, :last_name, :user_name, :email,:password)');
+foreach ($allUsers as $user) {
+    $stmt->bindValue(':user_name', $user['user_name'], PDO::PARAM_STR);
+    $stmt->bindValue(':first_name', $user['first_name'], PDO::PARAM_STR);
+    $stmt->bindValue(':last_name', $user['last_name'], PDO::PARAM_STR);
+    $stmt->bindValue(':email', $user['email'], PDO::PARAM_STR);
+    $stmt->bindValue(':password', $user['password'], PDO::PARAM_STR);
+    $stmt->execute();
+}
+
