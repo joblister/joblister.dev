45aceb709877f9b0a583a327fde5b0a120250480
Donald Moore <donmoore26762671@gmail.com>
2016-03-31 14:01:59 -0500

    Moved user, posts, comments migration files into one php file. Moved user and post seeders into one php file.

--- database/user_migration.php
+++ database/user_migration.php
@@ -2,8 +2,13 @@
 
 require_once 'joblister_db_connect.php';
 
+$dbc->exec('DROP TABLE IF EXISTS comments');
+
+$dbc->exec('DROP TABLE IF EXISTS posts');
+
 $dbc->exec('DROP TABLE IF EXISTS users');
-//create national_parks table columns
+
+
 $sql = <<<QUERY
 CREATE TABLE users(
 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -15,6 +20,54 @@ password VARCHAR(100) NOT NULL,
 PRIMARY KEY(id)
 
 )
+QUERY;
+$dbc->exec($sql);
+
+
+
+$sql = <<<QUERY
+CREATE TABLE posts(
+post_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+user_id INT UNSIGNED NOT NULL,
+title VARCHAR(100) NOT NULL,
+content VARCHAR (500) NOT NULL,
+date DATETIME,
+PRIMARY KEY(post_id),
+foreign key(user_id) references users(id)
+
+)
 
 QUERY;
 $dbc->exec($sql);
+
+
+
+
+ $sql = <<<QUERY
+ CREATE TABLE comments(
+ comment_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+ comment VARCHAR(500) NOT NULL,
+ post_id INT UNSIGNED NOT NULL,
+ user_id INT UNSIGNED NOT NULL,
+ date DATETIME,
+ foreign key(post_id) references posts(post_id),
+ foreign key(user_id) references users(id),
+ PRIMARY KEY(comment_id)
+ )
+
+QUERY;
+$dbc->exec($sql);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- database/users_seeder.php
+++ database/users_seeder.php
@@ -2,9 +2,18 @@
 require 'joblister_db_connect.php';
 require 'db_connect.php';
 // First, add a query to delete all the records from the table
-
-$truncate = 'TRUNCATE users';
+//CAN'T TRUNC A TABLE WITH A FOREIGN KEY
+$truncate = 'DELETE FROM posts';
 $dbc->exec($truncate);
+
+$truncate = 'DELETE FROM users';
+$dbc->exec($truncate);
+
+
+
+
+
+
 
 $Users = [
     ['first_name' => 'Don', 'last_name' => 'Moore','user_name' => 'donny71','email' => 'dmoore@jlister',
@@ -26,5 +35,22 @@ foreach ($Users as $user) {
     $stmt->bindValue(':password', $user['password'], PDO::PARAM_STR);
     $stmt->execute();
 }
+
+
+$posts = [
+    ['user_id' => 1,'title' => 'Javascript developer Needed in Austin!', 'content' => 'We are a new company looking for enthusiastic programmers to help us grow. We have plenty of work, but not enough programmers. Contact us at 555-5555.','date' => '2016-03-31 03:33:33'],
+
+    
+];
+$stmt = $dbc->prepare('INSERT INTO posts (user_id, title, content, date)
+    VALUES (:user_id, :title, :content,:date)');
+foreach ($posts as $post) {
+    $stmt->bindValue(':user_id', $post['user_id'], PDO::PARAM_INT);
+    $stmt->bindValue(':title', $post['title'], PDO::PARAM_STR);
+    $stmt->bindValue(':content', $post['content'], PDO::PARAM_STR);
+    $stmt->bindValue(':date', $post['date'], PDO::PARAM_STR);
+    $stmt->execute();
+}
+
 
 ?>
