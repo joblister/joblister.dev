9c8098381dd94d9c14dfd4e962204ed0f9ed3b11
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-03-31 14:38:17 -0500

    update on sign up form, not finished but cleaned up the bugs and out of place elements

--- public/users.create.php
+++ public/users.create.php
@@ -13,25 +13,37 @@
 	<link rel="stylesheet" type="text/css" href="/css/joblister.css">
 	<meta charset="UTF-8">
 	<title>Sign-Up</title>
+	<style>
+		body {
+			overflow:hidden;
+		}
+
+	</style>
 </head>
 <body>
 	<?php include 'partials/navbar.php';?>
-	
-	<div class="sign-up-form">
+
+
+
+	<div class="sign-up-form">	
+		<div class="sign-up-txt">
+		<h1 id="sign-up-direction">Sign Up</h1>
+
+		</div>	
 		<form method="POST">
 			<h3 class="sign-placeholders">First Name</h3>
-		    <input type="text" class="form-control "  aria-describedby="basic-addon1">
+		    <input type="text" class="form-control form1"  aria-describedby="basic-addon1">
 		    <h3 class="sign-placeholders">Last Name</h3>
-		    <input type="text" class="form-control"  aria-describedby="basic-addon1">
+		    <input type="text" class="form-control form1"  aria-describedby="basic-addon1">
 		    <h3 class="sign-placeholders">Email</h3>
-		    <input type="text" class="form-control"  aria-describedby="basic-addon1">
+		    <input type="text" class="form-control form1"  aria-describedby="basic-addon1">
 		    <h3 class="sign-placeholders">Username</h3>
-		    <input type="text" class="form-control"  aria-describedby="basic-addon1">
+		    <input type="text" class="form-control form1"  aria-describedby="basic-addon1">
 		    <h3 class="sign-placeholders">Password</h3>
-		    <input type="password" class="form-control"  aria-describedby="basic-addon1">
+		    <input type="password" class="form-control form1"  aria-describedby="basic-addon1">
 		     <h3 class="sign-placeholders">Confirm Password</h3>
-		    <input type="password" class="form-control"  aria-describedby="basic-addon1">
-		    <button  id="sign-up-btn" type="button" class="btn btn-default">sign up</button>
+		    <input type="password" class="form-control form1"  aria-describedby="basic-addon1">
+		    <button  id="profile-create" type="button" class="btn btn-default">sign up</button>
 
 	</div>
 
