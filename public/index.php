0795cb64808e71dee97cde7f07d13156144af59a
Donald Moore <donmoore26762671@gmail.com>
2016-04-08 10:01:21 -0500

    Worked on index page for demo by adding sneak peek pics into joblister.

--- public/index.php
+++ public/index.php
@@ -12,17 +12,17 @@ if(isset($_SESSION['logged_in_user'])){
 <html lang="en">
 <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job Lister</title>
-  <!-- order matters, my own stylesheet must go unederneath -->
-  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'> 
-  <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="/css/joblister.css">
+    <!-- order matters, my own stylesheet must go unederneath -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'> 
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/joblister.css">
 
-  
-  <!---external personalized stylesheet-->
-  <style type="text/css">
+      
+      <!---external personalized stylesheet-->
+    <style type="text/css">
 
     * {
         margin: 0;
@@ -35,15 +35,23 @@ if(isset($_SESSION['logged_in_user'])){
       /* equal to footer height */
         margin-bottom: -142px; 
     }
+
     .page-wrap:after {
       content: "";
       display: block;
     }
+
     .site-footer, .page-wrap:after {
       height: 142px; 
     }
 
-   </style>  
+    h3{
+        text-align: center;
+        text-decoration: underline;
+        color: red;
+    }
+
+    </style>  
 
 </head>
 <body>
@@ -51,23 +59,34 @@ if(isset($_SESSION['logged_in_user'])){
     <?php include 'partials/navbar.php';?>
   
     <div class="jumbotron">
-      <h1>Job Lister has the job that you're looking for!</h1>
-      <p>This region's number one site for employers looking for qualified professionals.
+        <h1>Job Lister has the job that you're looking for!</h1>
+
+        <p>This region's number one site for employers looking for qualified professionals.
          Also, the region's number one site for job seekers from all fields.
-      </p> 
+        </p> 
+
+    </div> <!-- end jumbotron -->
+   
+    <div id="posts-screenshot">
+        <h3>Browse Job listings!</h3>
+        <div class="index-posts">
+            <img src="/img/posts.png">
+        </div>
+        <br>
+        <br>
+        <h3>Apply to the jobs that interest you!</h3>
+        <div class="index-posts">
+            <img src="/img/posts-details-comments.png">
+        </div>
     </div>
 
-   <!--  <div>
-
-      <div class="col-lg-12" id="posts-screenshot"></div>
-
-      <div class="col-lg-12" id="posts-details-comments"></div>
-
-    </div> -->
-
 </div> <!-- end page-wrap -->
-
+    <br>
+    <br>
+ 
+<div>
     <?php include 'partials/footer.php';?>
+</div>
   <!--- js here -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
